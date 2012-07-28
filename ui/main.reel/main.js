a0e4b1deda337dfacbbf4ef0f94d09a1df292fab
Mike Czepiel <mczepiel@motorola.com>
2012-07-27 21:56:21 -0700

    Add todoListController to manage todos
    Nothing visible yet, but there's plenty to talk about even so.

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -1,7 +1,28 @@
 var Montage = require("montage").Montage,
     Component = require("montage/ui/component").Component,
+    ArrayController = require("montage/ui/controller/array-controller").ArrayController,
+    Todo = require("core/todo").Todo,
     LOCAL_STORAGE_KEY = "todos-montage";
 
 exports.Main = Montage.create(Component, {
+
+    todoListController: {
+        serializable: false,
+        value: null
+    },
+
+    didCreate: {
+        value: function() {
+            this.todoListController = ArrayController.create();
+        }
+    },
+
+    createTodo: {
+        value: function(title) {
+            var todo = Todo.create().initWithTitle(title);
+            this.todoListController.addObjects(todo);
+            return todo;
+        }
+    }
 
 });
